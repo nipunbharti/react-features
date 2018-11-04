7531730fb754c0fafe74d6dffde514c6f2580456
nipunbharti <ced16i026@iiitdm.ac.in>
2018-11-05 01:06:00 +0530

    Same as last commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import LazySuspense from "./components/LazySuspense";
 import ErrorBoundariesCheck from "./components/ErrorBoundariesCheck";
+import TodoStateHook from './components/StateHook';
 import './App.css';
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
           <Route exact path="/" component={Home} />
           <Route path="/lazysuspense/" component={LazySuspense} />
           <Route path="/errorboundaries/" component={ErrorBoundariesCheck} />
+          <Route path="/statehook/" component={TodoStateHook} />
         </div>
       </BrowserRouter>
     </div>
